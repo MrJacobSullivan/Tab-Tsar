5e8b079198d62954bf69c11a408615ff0aa99da4
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-02-28 04:42:27 -0600

    Rethought background.js structure
    It actually prints to console now using commands in manifest.json.

--- background.js
+++ background.js
@@ -1,44 +1,16 @@
-chrome.commands.onCommand.addListener(function (command) {
-  chrome.windows.getCurrent(function (currentWindow) {
-    chrome.tabs.getSelected(null, function (currentTab) {
-      chrome.tabs.query(
-        {
-          active: false,
-          pinned: false,
-          windowId: currentWindow.id,
-        },
-        function (tabs) {
-          var tabIds = new Array(tabs.length)
-          for (var element in tabs) tabIds.push(element.id)
-          var tabsToDelete = new Array(0)
-
-          switch (command) {
-            case 'close-all-except':
-              for (var element of tabIds.filter(
-                (value) => value !== currentTab.id
-              ))
-                tabsToDelete.push(element)
-              break
-            case 'close-all-other-tabs':
-              for (var element of tabIds) tabsToDelete.push(element)
-              break
-            case 'close-right-tabs':
-              for (var element of tabsIds.slice(
-                tabIds.indexOf(currentTab.id + 1)
-              ))
-                tabsToDelete.push(element)
-              break
-            case 'close-left-tabs':
-              for (var element of tabIds.slice(
-                null,
-                tabIds.indexOf(currentTab.id)
-              ))
-                tabsToDelete.push(element)
-              break
-          }
-          chrome.tabs.remove(tabsToDelete)
-        }
-      )
-    })
-  })
+chrome.commands.onCommand.addListener((command) => {
+  switch (command) {
+    case 'close-tabs-all-except':
+      console.log('close all except')
+      break
+    case 'close-tabs-all-including':
+      console.log('close all other')
+      break
+    case 'close-tabs-right':
+      console.log('close tabs right')
+      break
+    case 'close-tabs-left':
+      console.log('close tabs left')
+      break
+  }
 })
