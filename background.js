4c875a0b4dd649eb1075d61539f5dfd8a802d5a2
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-02-28 04:51:49 -0600

    Added close-tabs-all-except functionality

--- background.js
+++ background.js
@@ -1,16 +1,38 @@
 chrome.commands.onCommand.addListener((command) => {
-  switch (command) {
-    case 'close-tabs-all-except':
-      console.log('close all except')
-      break
-    case 'close-tabs-all-including':
-      console.log('close all other')
-      break
-    case 'close-tabs-right':
-      console.log('close tabs right')
-      break
-    case 'close-tabs-left':
-      console.log('close tabs left')
-      break
-  }
+  chrome.windows.getCurrent((currentWindow) => {
+    chrome.tabs.getSelected(null, (currentTab) => {
+      chrome.tabs.query(
+        {
+          active: false,
+          pinned: false,
+          windowId: currentWindow.id,
+        },
+        (tabs) => {
+          const tabIds = new Array(tabs.length)
+          for (var tab of tabs) tabIds.push(tab.id)
+
+          const tabsToDelete = []
+
+          switch (command) {
+            case 'close-tabs-all-except':
+              for (var element of tabIds.filter(
+                (value) => value !== currentTab.id
+              ))
+                tabsToDelete.push(element)
+              break
+            case 'close-tabs-all-including':
+              console.log('close all other')
+              break
+            case 'close-tabs-right':
+              console.log('close tabs right')
+              break
+            case 'close-tabs-left':
+              console.log('close tabs left')
+              break
+          }
+          chrome.tabs.remove(tabsToDelete)
+        }
+      )
+    })
+  })
 })
